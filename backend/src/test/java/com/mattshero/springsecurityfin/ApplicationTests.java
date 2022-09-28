06b8b52a91e0af7a5d07d28e7ae17be3ec0a6a81
matthew-desktop <mattshero40@gmail.com>
2022-09-28 15:43:43 -0500

    init

---
+++ backend/src/test/java/com/mattshero/springsecurityfin/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mattshero.springsecurityfin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
